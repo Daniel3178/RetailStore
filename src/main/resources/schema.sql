aec43190356872c2b048113af607e539035f54a5
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2024-11-28 01:17:33 +0100

    added two dockerfiles

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+
+CREATE DATABASE product_inventory;
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    stock INT NOT NULL,
+    price NUMERIC(10, 2) NOT NULL,
+    tax_rate NUMERIC(4, 2) NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    expiry_date DATE,
+    category TEXT,
+    supplier TEXT
+);
