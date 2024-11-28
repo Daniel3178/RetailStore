2c2e2d671ab61348d83d762e18cf1ecb6274b733
Daniel3178 <Daniel.ibrahimi360@gmail.com>
2024-11-28 11:57:31 +0100

    created a docker-compose file

--- Dockerfile.java
+++ Dockerfile.java
@@ -1,10 +1,10 @@
 FROM maven:latest AS builder
 WORKDIR /app
-COPY pom.xml .5 
+COPY pom.xml . 
 COPY src ./src
 RUN mvn clean package
 
 FROM openjdk:latest
 WORKDIR /app 
 COPY --from=builder /app/target/RetailStore-1.0-SNAPSHOT.jar .  
-CMD ["java", "-jar", "SoundGoodMusicDB-1.0-SNAPSHOT.jar"] 
+CMD ["java", "-jar", "RetailStore-1.0-SNAPSHOT.jar"] 
